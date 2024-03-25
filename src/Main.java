03b810be69786d1ee912509b1d847351a56e93a9
Pierre-Louis <pierrelouis.thomas@edu.ece.fr>
2024-03-25 14:52:43 +0100

    25/03

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Test de PL");
+        System.out.println("Test de Nina");
     }
 }
